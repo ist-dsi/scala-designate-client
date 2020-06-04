4b48ab81a1d1c1561b82ae0b15975f8c875330e8
Afonso Matos <afonsolfmatos@gmail.com>
2020-06-04 11:28:18 +0100

    Remove config file from git

--- openstack-openrc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export OS_USERNAME="admin"
-export OS_USER_DOMAIN_NAME="default"
-export OS_PASSWORD="oJiCK3dhiZ6TilT64Pl9B72tfjtNHpla"
-export OS_PROJECT_NAME="admin"
-export OS_PROJECT_DOMAIN_NAME="default"
-export OS_IDENTITY_API_VERSION="3"
-export OS_AUTH_URL=http://centos-sentmwlu.example-service-dev.al.vps.tecnico.ulisboa.pt:5000
-export OS_REGION_NAME="RegionT"
