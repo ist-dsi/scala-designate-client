39b0a7907287caeb7f2046aaba60c8c68cfbd63f
Afonso Matos <afonsolfmatos@gmail.com>
2020-06-25 20:34:30 +0100

    Remove crudService composition

--- src/main/scala/pt/tecnico/dsi/designate/services/ZoneTransferRequests.scala
+++ src/main/scala/pt/tecnico/dsi/designate/services/ZoneTransferRequests.scala
@@ -11,25 +11,19 @@ import pt.tecnico.dsi.designate.models.{Status, WithId, ZoneTransferRequest, Zon
 final class ZoneTransferRequests[F[_]: Sync](baseUri: Uri, authToken: Header)(implicit client: Client[F])
   extends BaseService[F](authToken) {
 
-  // We cannot extend CRUDService directly because `create` does not belong to uri
-  def crudService: AsymmetricCRUDService[F, ZoneTransferRequest] =
-    new AsymmetricCRUDService[F, ZoneTransferRequest](baseUri, name = "transfer_request", authToken) {
-      override type Update = ZoneTransferRequestUpdate
-  }
+  override val uri: Uri = baseUri / "transfer_requests"
 
-  def get(id: String): F[WithId[ZoneTransferRequest]] = crudService.get(id)
+  def list(query: Query = Query.empty): Stream[F, WithId[ZoneTransferRequest]] = super.list[WithId[ZoneTransferRequest]]("transfer_requests", uri, query)
 
-  def delete(id: String): F[Unit] = delete(uri / id)
+  def get(id: String): F[WithId[ZoneTransferRequest]] = super.get(uri / id)
 
-  def list(query: Query): Stream[F, WithId[ZoneTransferRequest]] = crudService.list(query)
-  def list(): Stream[F, WithId[ZoneTransferRequest]] = crudService.list()
+  def delete(value: WithId[ZoneTransferRequest]): F[Unit] = delete(value.id)
+  def delete(id: String): F[Unit] = super.delete(uri / id)
 
   def list(status: Option[Status]): Stream[F, WithId[ZoneTransferRequest]] =
-    crudService.list(Query.fromPairs("status" -> status.asJson.toString()))
+    list(Query.fromPairs("status" -> status.asJson.toString()))
 
-  // TODO: Couldn't use `crudService.update` because it relies on private type `Update`
   def update(id: String, value: ZoneTransferRequestUpdate)(implicit c: Codec[ZoneTransferRequestUpdate]): F[WithId[ZoneTransferRequest]]
     = super.update(uri / id, value)
 
-  override val uri: Uri = baseUri / "transfer_requests"
 }
